194ad5e097851bb86defd1756c11744c1e7dfef0
Nikson9898 <97338733+Nikson9898@users.noreply.github.com>
2022-01-22 19:35:29 +0530

    new

---
+++ additioninjava/src/birthdayreminder/informm.java
@@ -0,0 +1,30 @@
+package birthdayreminder;
+import java.time.LocalDate;
+import java.time.Month;
+
+public class informm
+{
+public static void main(String[] args) 
+{
+	{
+		int birthDate =29;
+		Month birthMonth=Month.SEPTEMBER;
+		LocalDate currentDate = LocalDate.now();
+	System.out.println("Todays Date:" +currentDate)	;
+	int date = currentDate.getMonth();
+	Monthmasam = CurerntDate.getMonth();
+	if(date==birthDate&&masam==birthMonth)
+	{
+		System.out.println("happy birthday to you");
+	}
+	else
+	{
+		System.out.println ("today is not my birth day");
+	}
+	}
+}
+ 
+
+  
+}
+
